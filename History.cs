9e06878ff1e8cdc8f1746954e9f424ab8baf4c0e
Brent Ferree <brferree@allegisgroup.com>
2019-11-24 20:42:04 -0500

    Add project files.

---
+++ History.cs
@@ -0,0 +1,46 @@
+﻿using CsvHelper.Configuration.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SeratoHistoryMerge
+{
+    class History
+    {
+        public string name { get; set; }
+
+        public string artist { get; set; }
+
+        public string bpm { get; set; }
+
+
+        [Name("start time")]
+        public string starttime { get; set; }
+
+
+        [Name("end time")]
+        public string endtime { get; set; }
+
+        public string playtime { get; set; }
+
+
+        public string deck { get; set; }
+
+        public string notes { get; set; }
+       
+        
+        public string album { get; set; }
+
+        public string filename { get; set; }
+
+        public string location { get; set; }
+
+        public string comment { get; set; }
+
+
+
+
+    }
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using CsvHelper;
+using System;
+using System.IO;
+
+namespace SeratoHistoryMerge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            string[] myFiles = Directory.GetFiles(@"c:\\SeratoHistory\\", "*.csv");
+            DateTime startingDate;
+            DateTime endingDate;
+            DateTime rowDate;
+            
+            foreach (string filename in myFiles)
+            {
+                using (var reader = new StreamReader(filename))
+                using (var csv = new CsvReader(reader))
+                {
+                    csv.Configuration.PrepareHeaderForMatch = (string header, int index) => header.ToLower();
+                    using (var historydata = new CsvReader(reader))
+                    {
+                        int rowCount = 0;
+
+                        var records = historydata.GetRecords<dynamic>();
+
+
+                    }
+
+                }
+            }
+        }
+    }
+}
